4090d6c2f03067bd6578e19830ea545ba7cfb903
Martin Fylke <martin.fylke@volvocars.com>
2020-09-29 22:03:05 +0200

    layout proposition

--- src/App.js
+++ src/App.js
@@ -3,10 +3,72 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{height:100 + '%'}}>
+    <div style={{ height: 100 + '%' }}>
       <div class="bg">
       </div>
-      <p>Välkommen till vårt bröllop osv.</p>
+      <div className="intro-text">
+        <h1>VÄLKOMMEN TILL #FYLKE2020</h1>
+        <p>Vi ser väldigt mycket fram emot att fira vår stora dag tillsammans med dom vi tycker om allra mest!</p>
+
+        <p>Det blir en helhelg med start på fredagen med mingel och härligt häng. Vigseln äger rum på lördagen med efterföljande middag och fest.</p>
+      </div>
+      <div className="information-text">
+        <h1>Viktig information</h1>
+        <strong>Klädkod</strong>
+        <p>Kavaj</p>
+        <br />
+        <strong>Boende</strong>
+        <p>På ön finns boende för alla gäster och är bokat och klart (vi är de enda som huserar på ön under helgen). Boendet är fördelat i mindre stugor och större hus. När ni kommer till ön och checkar in får ni reda på var ni kommer att bo. Vi kommer att fördela er på bästa möjliga sätt :) Boendet kostar 500 kr/natt och person och betalas på plats.</p>
+        <br />
+        <strong>Mat</strong>
+        <p>Frukost ingår hela helgen. Det finns inga butiker eller andra matställen på ön än värdshuset. De fixar lunch till gästerna på lördagen som kostar runt en hundralapp.</p>
+        <br />
+        <strong>Barn</strong>
+        <p>Ni vet att vi älskar barn men just denna helg vill vi fira med våra vuxna nära och kära. Ammande barn är givetvis välkomna.</p>
+        <br />
+        <strong>Presenter</strong>
+        <p>För oss är den största gåvan att alla vi tycker bäst om kommer och firar vår stora dag. Om ni ändå vill uppvakta oss, så önskar vi oss bidrag till vår bröllopsresa.</p>
+        <p>Swish: 0737 78 94 33</p>
+        <br />
+        <strong>Tänk på att</strong>
+        <p>Vigselplatsen är i naturen precis vid vattnet. Så för alla tjejer som planerar att ha stilettklackar är tips att ta med ett par extra lågskor att byta om till vid vigseln.</p>
+        <em>Hur du hittar till Stenungsön hittar du längre ner på sidan.</em>
+      </div>
+      <div className="album">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>Bilder på Alicia och Anton</h1>
+      </div>
+      <div className="location">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>Hitta till Stenungsön</h1>
+      </div>
+      <div className="program">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>Program</h1>
+      </div>
+      <div className="toastmasters">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>Toastmasters</h1>
+      </div>
+      <div className="osa">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>O.S.A</h1>
+      </div>
     </div>
   );
 }
