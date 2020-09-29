fdeddaf2e7fb0ef624235482e3e862d78d45b8c5
Martin Fylke <martin.fylke@volvocars.com>
2020-09-29 21:30:12 +0200

    text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div style={{height:100 + '%'}}>
       <div class="bg">
       </div>
-      <p>This example creates a full page background image. Try to resize the browser window to see how it always will cover the full screen (when scrolled to top), and that it scales nicely on all screen sizes.</p>
+      <p>Välkommen till vårt bröllop osv.</p>
     </div>
   );
 }
