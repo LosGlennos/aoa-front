60721aca911c5f96c62c0ae8a2a80973af9fb5d0
Martin Fylke <martin.fylke@volvocars.com>
2021-01-21 10:41:11 +0100

    fixed some texts

--- src/information/Information.js
+++ src/information/Information.js
@@ -8,23 +8,33 @@ export default function Information() {
             <Container textAlign="center">
                 <h1 className='heading-text'>Viktig information</h1>
                 <strong>Klädkod</strong>
+                <br/>
+                <br/>
                 <p>Kavaj</p>
                 <br />
                 <strong>Boende</strong>
+                <br/>
+                <br/>
                 <p>På Stenungsögården finns det sovplatser reserverat för alla. För att du ska få just det boende du vill, vänligen kontakta receptionen på deras mejl eller telefonnummer.</p>
                 <p>Vänta inte med att boka då det är lite först till kvarn. På Stenungsöngårdens hemsida kan ni se bilder, info och priser för de olika alternativen. Ert boende betalas på plats när ni checkar in.</p>
                 <p>Kursgården och Pensionat är ej bokningsbara.</p>
                 <p><a href="http://www.stenungsogarden.se/boende/">http://www.stenungsogarden.se/boende/</a></p>
                 <br />
                 <strong>Mat</strong>
+                <br/>
+                <br/>
                 <p>På fredagen bjuds det på grillat för er som är med oss.</p>
                 <p>Frukost på lördagen samt brunchen på söndagen meddelas vid er bokning av boende och betalas på plats.</p>
                 <p>Glöm inte att fylla i eventuella allergier, "tycker inte om fisk", specialkost etc.</p>
-                <br />
+                <br/>
                 <strong>Barn</strong>
+                <br/>
+                <br/>
                 <p>Ni vet att vi älskar barn men just denna helg vill vi fira med våra vuxna nära och kära. Ammande barn är givetvis välkomna.</p>
                 <br />
                 <strong>Presenter</strong>
+                <br/>
+                <br/>
                 <p>För oss är den största gåvan att alla vi tycker bäst om kommer och firar vår stora dag.</p>
                 <p>Om ni ändå vill uppvakta oss så önskar vi oss ett bidrag till vår bröllopsresa.</p>
                 <p>Era eventuella bidrag är såklart anonyma.</p>
@@ -32,8 +42,10 @@ export default function Information() {
                 <p>Bankkonto, 8105-9, 973 352 275-0</p>
                 <br />
                 <strong>Tänk på att</strong>
-                <p>Till och från vigseln är det en kort promenad på en grusväg. Så tjejer eller killar, vi har inga fördomar.</p>
-                <p>Tänk på klackarna, ta med ett skoombyte som kan användas till kvällens dans.</p>
+                <br/>
+                <br/>
+                <p>Till och från vigseln är det en kort promenad på en grusväg.</p>
+                <p>Så tjejer eller killar, vi har inga fördomar, tänk på klackarna. Ta med ett skoombyte som kan användas till kvällens dans.</p>
                 <em>Hur du hittar till Stenungsön hittar du längre ner på sidan.</em>
             </Container>
         </div>
--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -7,9 +7,9 @@ export default function Intro() {
         <div className="dark-green-theme container-padding-2em">
             <Container textAlign="center">
                 <h1 className='heading-text'>VÄLKOMMEN TILL #FYLKE2021</h1>
-                <p>Vi ser väldigt mycket fram att fira vår stora dag tillsammans med er vi tycker om allra mest!</p>
+                <p>Vi ser väldigt mycket fram emot att fira vår stora dag tillsammans med er vi tycker om allra mest!</p>
                 <br />
-                <p>Det blir en helghelg med start på fredag för alla som kan där vi planerar för ett trevligt häng och grillkväll.</p>
+                <p>Det blir en helhelg med start på fredagen för alla som kan där vi planerar för ett trevligt häng och grillkväll.</p>
                 <p>Vigseln äger rum på lördagen med efterföljande middag och fest.</p>
                 <p>Söndagen avslutas med en brunch.</p>
                 <br />
